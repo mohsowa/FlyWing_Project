fb89d9ce495801fde20ce1bf1e221f33cf3a567b
mohsowa <mohammed240owa@gmail.com>
2022-04-06 05:33:30 +0300

    Last Edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,18 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <h3 class="card-header text-center">{{ __('Login') }}</h3>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">{{ __('Email Address') }}</label>
+                            <div class="">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -25,10 +24,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -40,7 +39,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-lg d-flex align-self-center order-lg-first order-md-first order-sm-last ">
-            <div class="container text-center">
-                <div class="">
-                    <h1 class="h1 display-5 text-uppercase">{{__('START TRAVELING WITH US')}}</h1>
+    <section class="heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-5 col-lg-5 col-xl-5 col-xxl-5 d-flex order-md-first align-self-center">
+                    <div class="container text-center">
+                        <div class="">
+                            <h1 class="h1 display-5 text-uppercase">{{__('START TRAVELING WITH US')}}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-7 col-lg-7 col-xl-7 col-xxl-7 order-first">
+                    <div class="plane">
+                        <img src="{{ asset('src/images/plane/plane.png') }}" class="img-fluid" alt="plane">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm order-sm-first ">
-            <div class="plane">
-                <img src="{{ asset('src/images/plane/plane.png') }}" class="img-fluid" alt="plane">
-            </div>
+    </section>
+
+    <section>
+        <div class="container">
+
         </div>
-    </div>
-</div>
+    </section>
+
 @endsection
