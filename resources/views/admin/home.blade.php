11af1f31ab57a71f7d66b1146fb355a08dd42f04
mohsowa <mohammed240owa@gmail.com>
2022-04-11 07:20:04 +0300

    Edit Admin Home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,30 @@
-<h1> Admin Page</h1>
+{{-- Main Admin Nav--}}
+
+<div class="container ">
+    <ul class="nav nav-tabs section_nav_tab" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true"><i class="fa-solid fa-user-gear"></i> {{__('Admins')}}</button>
+        </li>
+        <li class="nav-item " role="presentation">
+            <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false"><i class="fa-solid fa-users"></i>  {{__('Passengers')}}</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false"><i class="fa-solid fa-calendar-check"></i>  {{__('Flights')}}</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false"><i class="fa-solid fa-credit-card"></i>  {{__('Payments')}}</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false"><i class="fa-solid fa-ticket"></i> {{__('Tickets')}}</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false"><i class="fa-solid fa-server"></i> {{__('Site Settings')}}</button>
+        </li>
+
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,17 @@
 @section('page_title') {{__('HOME')}} @endsection
 
 @section('content')
-<div class="container py-5">
+<div class="container py-4 p-5">
     <div class="">
         <h3 class="h3">{{__('Welcome')}}, {{ Auth::user()->Fname  }} {{ Auth::user()->Lname  }}</h3>
     </div>
-    <div>
-
-
-    </div>
 </div>
+
+{{-- Open Admin Page --}}
+@if(App\Models\Admin::where('user_id', Auth::user()->id)->exists())
+    @include('admin.home')
+@endif
+
+
+
 @endsection
