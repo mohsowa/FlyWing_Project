0955d7f2b904c0a49182304db8f5857ad8c3b2e6
mohsowa <mohammed240owa@gmail.com>
2022-04-11 04:28:03 +0300

    from local

---
+++ app/Http/Controllers/TicketForPaymentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TicketForPayment;
+use Illuminate\Http\Request;
+
+class TicketForPaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TicketForPayment  $ticketForPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TicketForPayment $ticketForPayment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TicketForPayment  $ticketForPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TicketForPayment $ticketForPayment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TicketForPayment  $ticketForPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TicketForPayment $ticketForPayment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TicketForPayment  $ticketForPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TicketForPayment $ticketForPayment)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UsePlaneController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\usePlane;
+use Illuminate\Http\Request;
+
+class UsePlaneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\usePlane  $usePlane
+     * @return \Illuminate\Http\Response
+     */
+    public function show(usePlane $usePlane)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\usePlane  $usePlane
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(usePlane $usePlane)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\usePlane  $usePlane
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, usePlane $usePlane)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\usePlane  $usePlane
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(usePlane $usePlane)
+    {
+        //
+    }
+}
